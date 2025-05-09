57d0d7fda1ef3956b18b1fca9d9b32ef35a81929
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-05-09 00:27:06 -0400

    fix/counting sort remove macros

--- src/modules/sorting/counting_sort.cpp
+++ src/modules/sorting/counting_sort.cpp
@@ -1,27 +1,25 @@
 #include "counting_sort.h"
-#include <bits/stdc++.h>
-using namespace std;
-typedef vector<int> vi;
-#define sz(x) (int)(x).size()
-#define forn(i, n) for (int i = 0; i < (n); i++)
-#define fore(i, a, b) for(int i = (a); i<(b); i++)
-
-int encontrarMax(vi& arr){
+#include <vector>
+int encontrarMax(std::vector<int>& arr){
     int max = arr[0];
-    fore(i, 1, sz(arr)){
+    int n = (int)arr.size();
+    for(int i = 1; i<n; i++){
         if(arr[i] > max) max = arr[i];
     }
     return max;
 }
 
-vi counting_sort(vi arr){
-    vi res(sz(arr));
+std::vector<int> counting_sort(std::vector<int> arr){
+    int n = (int)arr.size();
+    std::vector<int> res(n);
     int max = encontrarMax(arr);
-    vi frec(max + 1);
+    std::vector<int> frec(max + 1);
 
     for(int x:arr) frec[x]++;
-    fore(i,1,(max+1)) frec[i] += frec[i -1];
-    for(int i = sz(arr) - 1; i >= 0; i--){
+    for(int i = 1; i<=max ; i++){
+        frec[i]+= frec[i - 1];
+    }
+    for(int i = n - 1; i >= 0; i--){
         int ind = frec[arr[i]];
         res[ind -1] = arr[i];
         frec[arr[i]]--;
